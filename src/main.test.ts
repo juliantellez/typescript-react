d510f5faad88f2223cf5738b4f32a5b9a23e8cc3
Julian <juliantellezmendez@gmail.com>
2020-02-01 19:55:45 +0000

    chore(dependencies): adds jest

---
+++ src/main.test.ts
@@ -0,0 +1,8 @@
+describe("Example description", () => {
+  it("should resolve an expectation", () => {
+    const value = "foo";
+    const expected = "foo";
+
+    expect(value).toBe(expected);
+  });
+});
