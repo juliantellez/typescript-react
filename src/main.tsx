852563a91856ee4fc63e548d31c2f90ab23ee5b3
Julian <juliantellezmendez@gmail.com>
2020-02-01 19:13:47 +0000

    chore(dependencies): react + dom

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const App = () => <div>Hello World</div>;
+
+const container = document.getElementById("main");
+ReactDOM.render(<App />, container);
