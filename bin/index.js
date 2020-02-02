399a703ea8856bd290592a027dcb6edf86d19cad
Julian <juliantellezmendez@gmail.com>
2020-02-02 23:55:38 +0000

    test(npx): use inquirer and chalk

--- bin/index.js
+++ bin/index.js
@@ -3,74 +3,19 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-require('yargs')
-    .scriptName('typescript-react')
-    .usage('$0 <cmd> [args]')
-    .command(
-        'install [name]',
-        'bootstrap dependencies',
-        yargs => {
-            yargs.positional('name', {
-                type: 'string',
-                default: 'typescript-react-boilerplate',
-                describe: 'directory name',
-            })
+const renderBanner = require('./banner')
+const inquirer = require('inquirer')
+const start = require('./start')
+
+renderBanner()
+
+inquirer
+    .prompt([
+        {
+            name: 'projectName',
+            message: 'What would you like to call this project?',
+            default: 'typescript-react-boilerplate',
+            validate: input => Boolean(input.length),
         },
-        async args => {
-            const fs = require('fs')
-            const util = require('util')
-
-            console.log('CURRENT_DIR:', process.cwd())
-
-            const ROOT = process.cwd()
-            const directory = ROOT + '/' + args.name
-
-            if (!fs.existsSync(directory)) {
-                console.log('Creating folder: ' + directory)
-                fs.mkdirSync(directory)
-            } else {
-                console.log('Directory already exists')
-                process.exit(0)
-            }
-
-            process.chdir(directory)
-            const exec = util.promisify(require('child_process').exec)
-
-            try {
-                const { stdout: npmInit } = await exec('npm init --yes')
-                console.log('[NPM INIT]', npmInit)
-
-                const { stdout: npmInstall } = await exec(
-                    'npm i typescript-react@latest --save-dev'
-                )
-                console.log('[NPM INSTALL]', npmInstall)
-
-                const packageJson = require(`${directory}/package.json`)
-
-                const { stdout: cpDir } = await exec(
-                    `cp -R ${directory}/node_modules/typescript-react/{.,}* ${directory}`
-                )
-                console.log('[COPY BOILERPLATE]', cpDir)
-
-                const boilerplateJson = require(`${directory}/node_modules/typescript-react/package.json`)
-
-                const obj = {
-                    ...packageJson,
-                    scripts: boilerplateJson.scripts,
-                    devDependencies: boilerplateJson.devDependencies,
-                }
-
-                fs.writeFileSync(
-                    `${directory}/package.json`,
-                    JSON.stringify(obj)
-                )
-
-                const { stdout: npmInstallDeps } = await exec('npm install')
-                console.log('[NPM INSTALL BOILERPLATE] ', npmInstallDeps)
-            } catch (e) {
-                console.log(e)
-                process.exit(1)
-            }
-        }
-    )
-    .help().argv
+    ])
+    .then(start)
---
+++ bin/start.js
@@ -0,0 +1,104 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
+const fs = require('fs')
+const util = require('util')
+const chalk = require('chalk')
+const exec = util.promisify(require('child_process').exec)
+
+/**
+ *
+ * @param {string} projectName
+ * @param {string} directoryPath
+ */
+const createProjectDir = (projectName, directoryPath) => {
+    const directory = directoryPath + '/' + projectName
+
+    if (!fs.existsSync(directory)) {
+        console.log(chalk.yellow('Creating folder: ' + directory))
+        fs.mkdirSync(directory)
+    } else {
+        console.log(chalk.red('Directory already exists'))
+        process.exit(0)
+    }
+}
+
+/**
+ *
+ * @param {string} projectName
+ */
+const main = async projectName => {
+    try {
+        createProjectDir(projectName, process.cwd())
+        /**
+         * Move to current dir
+         */
+        process.chdir(directory)
+
+        /**
+         * Create Package.json
+         */
+        const { stdout: init } = await exec('npm init --yes')
+        console.log(chalk.yellow('[NPM INIT]'), chalk.gray(init))
+
+        /**
+         * Install latest version
+         */
+        const { stdout: install } = await exec(
+            'npm i typescript-react@latest --save-dev'
+        )
+        console.log(chalk.yellow('[NPM INSTALL]'), chalk.gray(install))
+
+        /**
+         * Copy dependencies to current dir
+         */
+        const packageJson = require(`${directory}/package.json`)
+        const { stdout: copy } = await exec(
+            `cp -R ${directory}/node_modules/typescript-react/{.,}* ${directory}`
+        )
+        console.log(chalk.yellow('[COPY BOILERPLATE]'), chalk.gray(copy))
+
+        /**
+         * Aggregate Json files
+         */
+        const boilerplateJson = require(`${directory}/node_modules/typescript-react/package.json`)
+        const unifiedPackageJson = {
+            ...packageJson,
+            scripts: boilerplateJson.scripts,
+            devDependencies: boilerplateJson.devDependencies,
+        }
+
+        fs.writeFileSync(
+            `${directory}/package.json`,
+            JSON.stringify(unifiedPackageJson)
+        )
+
+        /**
+         * Install dependencies
+         */
+        const { stdout: installDeps } = await exec('npm install')
+        console.log(
+            chalk.yellow('NPM INSTALL BOILERPLATE]'),
+            chalk.gray(installDeps)
+        )
+
+        /**
+         * Uninstall typescript-react
+         */
+        const { stdout: uninstall } = await exec(
+            'npm uninstall typescript-react'
+        )
+        console.log(
+            chalk.yellow('NPM UNINSTALL BOILERPLATE]'),
+            chalk.gray(uninstall)
+        )
+    } catch (e) {
+        console.log(chalk.red(e))
+        process.exit(1)
+    }
+}
+
+module.exports = main
