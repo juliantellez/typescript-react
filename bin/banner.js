57187225eb52810f789e5ab5b2ce9bea2bb95e0f
Julian <juliantellezmendez@gmail.com>
2020-02-03 21:03:52 +0000

    fix(npx): recursive node_module copy

--- bin/banner.js
+++ bin/banner.js
@@ -13,14 +13,15 @@ const logo = fs.readFileSync(path.join(__dirname, './ascii.txt'), 'utf8')
 const example = chalk.yellow('npx typescript-react hello-world')
 
 const main = () => {
-    console.log(`\n ${chalk.yellow(logo)}`)
+    console.log(`\n${chalk.yellow(logo)}`)
 
     // eslint-disable-next-line prettier/prettier
 console.log(`
 ------------------------------------------------------------------------------
 ------------------------------------------------------------------------------
 
-Version: ${packageJson.version}
+Version: ${chalk.yellow(packageJson.version)}
+Contribute: ${chalk.yellow(packageJson.homepage)}
 
 Use this tool with npx e.g ${example}
 
