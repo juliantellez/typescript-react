9327c40ab5b0ab14f1e34e6dc42d034b55b6d4d0
Julian <juliantellezmendez@gmail.com>
2020-04-25 22:12:06 +0100

    chore(ci-test): fetch from url

--- bin/config.js
+++ bin/config.js
@@ -5,7 +5,7 @@ const Env = {
 }
 
 const config = {
-    REPOSITORY_NAME: process.env.REPOSITORY_NAME || "git@github.com:juliantellez/typescript-react.git",
+    REPOSITORY_NAME: process.env.REPOSITORY_NAME || "https://github.com/juliantellez/typescript-react.git",
     REPOSITORY_BRANCH: process.env.REPOSITORY_BRANCH || "master",
     PROJECT_NAME: process.env.PROJECT_NAME || "test",
     NODE_ENV: process.env.NODE_ENV || Env.DEV
